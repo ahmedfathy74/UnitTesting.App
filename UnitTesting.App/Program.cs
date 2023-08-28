eef8cb17cf9ee3b2b314c1a35e6cd8ad1f3facba
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2023-08-28 16:14:08 +0300

    Add project files.

---
+++ UnitTesting.App/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using SW.Payroll;
+using System.Diagnostics;
+
+namespace UnitTesting.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                Id = 1000,
+                Name = "Reem A.",
+                DutyStation = "Montreal",
+                IsMarried = true,
+                TotalDependancies = 2,
+                Wage = 910m,
+                WorkingDays = 22,
+                IsDanger = true,
+                HasPensionPlan = true,
+                HealthInsurancePackage = HealthInsurancePackage.Basic,
+                WorkPlatform = WorkPlatform.Hybrid
+            };
+
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+            var empSalarySlip = salarySlipProcessor.Process(employee);
+            Console.WriteLine(empSalarySlip);
+            Console.ReadKey();
+
+        }
+        
+    }
+}
